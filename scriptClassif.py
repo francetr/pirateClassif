c8b651296cc43952e1f9dac274e3ba39dff2faf6
francetr <tristan.frances@laposte.net>
2018-03-30 15:49:31 +0200

    ajout print etape

--- scriptClassif.py
+++ scriptClassif.py
@@ -5,6 +5,8 @@ from Bio import SeqIO # For the fasta reading
 import argparse
 import re
 
+
+
 ############
 #   Commandes to launch this script :
 #                   python3 <path/toward/this/script> <path/toward/the/classif/file> <path/toward/the/fasta/file>
@@ -18,7 +20,7 @@ def retrieveArguments():
     """
     Retrieve the arguments from the command line
 
-    :return parser: attributes NameSpace of object argParse that contain the two argument name
+    :return parser: attributes NameSpace of object argParse that contain the two arguments name
 
     """
     ###     Mananage the 2 arguments (PASTEC and FASTA file name) when the command is launched
@@ -27,11 +29,11 @@ def retrieveArguments():
     parser.add_argument("classif", type=str, help="classif file that comes from from PASTEC")
     parser.add_argument("fasta", type=str, help="fasta file providing the sequence")
     args = parser.parse_args()
-    checkArgument(args.classif, args.fasta)
+    checkArguments(args.classif, args.fasta)
 
     return(args)
 
-def checkArgument(CLASSIFNAME, FASTANAME):
+def checkArguments(CLASSIFNAME, FASTANAME):
     """
     Check the extension of the two arguments of command line. If it is not correct, stop the programm.
 
@@ -44,16 +46,16 @@ def checkArgument(CLASSIFNAME, FASTANAME):
     """
     try:
         classifName=re.match(r'[\S]*[/]?[\w\-.]+(classif)$',CLASSIFNAME).groups()[0] ### regex checking is classif file as good extension
-        fastaName=re.match(r'[\S]*[/]?[\w\-.]+(fasta)$', FASTANAME).groups()[0] ### regex checking is fasta file as good extension
-        if classifName == "classif" :
-            # print("the path of {} is {}".format(classifName, answer))
-            pass
-        if fastaName == "fasta":
-            # print("the path of {} is {}".format(fastaName, args.fasta))
-            pass
     except AttributeError as e:
-        print("One of the extension file is incorrect")
+        print("###  Wrong extension for the classif file\n###   Classification aborted")
         sys.exit(1)
+
+    try:
+        fastaName=re.match(r'[\S]*[/]?[\w\-.]+(fasta)$', FASTANAME).groups()[0] ### regex checking is fasta file as good extension
+    except AttributeError as e:
+        print("###  Wrong extension for the fasta file\n###  Classification aborted")
+        sys.exit(1)
+
 
 
 def readPastec(PASTEC, NONTE, POTENTIALCHIMERIC, NOCAT, TE):
@@ -71,17 +73,18 @@ def readPastec(PASTEC, NONTE, POTENTIALCHIMERIC, NOCAT, TE):
 
     """
     try:
-        ### open the classif file
+        ###     open the classif file
         with open(PASTEC, "r") as f:
-            ### parse every line/sequence of the file
+            ###     parse every line/sequence of the file
             for line in f:
                 sequence=line.replace("\t\t", "\t").strip() # remove the first column double tab and carriage return of the line
                 categorization(sequence, NONTE, POTENTIALCHIMERIC, NOCAT, TE)
-            print("TE : {}\nnonTE : {}\nnoCat : {}\npotentialChimeric : {}\nTotal : {}".format(len(TE), len(nonTE), len(noCat), len(potentialChimeric), len(TE)+len(nonTE)+len(noCat)+len(potentialChimeric)))
+            print("Summary, sequences find by type :\nTE : {}\nnonTE : {}\nnoCat : {}\npotentialChimeric : {}\nTotal : {}".format(len(TE), \
+             len(nonTE), len(noCat), len(potentialChimeric), len(TE)+len(nonTE)+len(noCat)+len(potentialChimeric)))
 
-    except FileNotFoundError as e:
-        ### prevent the opening if the file name is incorrect
-        print("No such file as {}".format(PASTEC))
+    except FileNotFoundError:
+        ###     prevent the opening if the file name is incorrect
+        print("###  No such file as {}\n###  Classification aborted".format(PASTEC))
         sys.exit(1)
 
 def readFasta(FASTA):
@@ -101,7 +104,6 @@ def readFasta(FASTA):
     :return seqReturned: Dictionnary with key: id of the sequence and value : {"seq" : sequence of the FASTA sequence}
 
     """
-    # TODO find a way to save all the sequences, for now it read only one
     seqReturned={}
     try:
         ###   Open the fasta file
@@ -113,7 +115,7 @@ def readFasta(FASTA):
             return(seqReturned)
     except(FileNotFoundError, NameError):
         ### prevent the opening if the file name is incorrect
-        print("No such file as {}".format(FASTA))
+        print("###  No such file as {}\n###  Classification aborted".format(FASTA))
         sys.exit(1)
 
 def categorization(SEQUENCE, NONTE, POTENTIALCHIMERIC, NOCAT, TE):
@@ -137,17 +139,10 @@ def categorization(SEQUENCE, NONTE, POTENTIALCHIMERIC, NOCAT, TE):
     :return: None
 
     """
-    # NB : there seems to be \r\n character due to windows but it don't change the processing of the string if we split with \n or \r\n
-    # try :
-    #     SEQUENCE = SEQUENCE.split("\n") # split the original file into a list with each lines
-    # except :
-    #     pass
     listOrder=["SSR", "noCat", "LTR", "TRIM", "PotentialHost", "TIR", "DIRS", "LARD", "LINE", "MITE", "Helitron", "Maverick", "SINE"]
     features=SEQUENCE.split("\t")
     #TODO Treatment of the superfamily
     classDetermination(features, nonTE, potentialChimeric,  noCat, TE)
-    # TE[tmp[0]]={"class":tmp[4],"order":tmp[5], "other":tmp[7], "superfamily":"TODO", "autonomous":"TODO", "sequence":"TODO"}
-
 
 
 def classDetermination(SEQUENCE, NONTE, POTENTIALCHIMERIC, NOCAT, TE):
@@ -166,26 +161,27 @@ def classDetermination(SEQUENCE, NONTE, POTENTIALCHIMERIC, NOCAT, TE):
 
     """
     # typeOrder=None
-    ###   check first if the sequence is chimeric
+    ###     check first if the sequence is chimeric
     if(SEQUENCE[3] != "PotentialChimeric"):
-        ###   check the class of the sequence if it is not chimeric : class I
+        ###     check the class of the sequence if it is not chimeric : class I
         if(SEQUENCE[4] == "I"):
             TE[SEQUENCE[0]]={"class":"I"}
             # print(SEQUENCE[0])
             orderDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE)
             pass
-        ###   classII
+        ###     classII
         elif(SEQUENCE[4] == "II"):
             TE[SEQUENCE[0]]={"class":"II"}
             orderDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE)
             # print(SEQUENCE[0])
             pass
-        ###   noCat
+        ###     NoCat
         elif(SEQUENCE[4] == "noCat"):
+            ###   noCat
             NOCAT[SEQUENCE[0]]={"class":"noCat"}
             # print("NoCat : %s %s" % (SEQUENCE[4], SEQUENCE[5]))
             pass
-        ###   NonTE
+        ###     NonTE
         elif(SEQUENCE[4] == "NA"):
             NONTE[SEQUENCE[0]]={"class":"nonTE"}
             # print("NA : %s %s" % (SEQUENCE[4], SEQUENCE[5]))
@@ -213,22 +209,24 @@ def orderDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE):
     :return: None
 
     """
-    TE[SEQUENCE[0]]["order"]=SEQUENCE[5]
-    if (TE[SEQUENCE[0]]["order"]=="noCat"):
-        NOCAT[SEQUENCE[0]] = TE[SEQUENCE[0]]
+    if (SEQUENCE[5]=="noCat"):
+        ###     The order of the TE is not determined : the sequence will be put in NOCAT dictionnary and removed from the TE dictionnary
+        NOCAT[SEQUENCE[0]]={"class": SEQUENCE[4], "order": SEQUENCE[5]}
         del TE[SEQUENCE[0]]
         # print(NOCAT[SEQUENCE[0]])
 
     else:
+        ###     The order of the TE is determined : the super family of the TE will be determined
+        TE[SEQUENCE[0]]["order"] = SEQUENCE[5]
         superFamilyDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE)
 
 def superFamilyDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE):
+    #TODO if no coding part, move to POTENTIALCHIMERIC or NOCAT dictionnary?
     """
     Determine the super family of a sequence. Doing so it complete a dictionnary
     containing the transposable element.
     If the superFamily can't be defined, it will be noCat
-    If there is 2 or more superfamily possible, the sequence will be added to the
-    POTENTIALCHIMERIC dictionnary and then removed from TEdictionnary
+    If there is 2 or more superfamily possible, the sequence will be added to the POTENTIALCHIMERIC dictionnary and then removed from TE dictionnary
 
     :param SEQUENCE: name of the list of strings, which contain the sequence to categorize, that will be parsed
     :param POTENTIALCHIMERIC: dictionnary for potential chimeric element
@@ -238,15 +236,16 @@ def superFamilyDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE):
     :return: None
 
     """
-    ### search if there is a 'coding' part in the 7th value of SEQUENCE => needed
-    ### to defined the SEQUENCE superfamily
     try:
+        ###     search if there is a 'coding' part in the 7th value of SEQUENCE => needed to defined the SEQUENCE superfamily
         codingRecord = re.search(r'coding=\(([^\)]+)\)', SEQUENCE[7]).groups()[0]
         # print(codingRecord)
     except AttributeError:
+        ###     there is no coding part : the sequence is put in the POTENTIALCHIMERIC dictionnary and removed from th TE dictionnary
         # print(SEQUENCE[7])
         POTENTIALCHIMERIC[SEQUENCE[0]]=TE[SEQUENCE[0]]
         del TE[SEQUENCE[0]]
+        return
 
     # databaseRecords = codingRecord.split(';')
     # matches = {}
@@ -295,17 +294,23 @@ def save(FASTA, NONTE, POTENTIALCHIMERIC, NOCAT, TE):
 if __name__ == "__main__":
     # nom="/home/tfrances/Bureau/donnees/sortie_PASTEC/TisoMgescan.classif"
 
+    print("Start of the classification\n")
     ###     Instanciation of dictionnaries that will contain the results
     nonTE, potentialChimeric,  noCat, TE = {}, {}, {}, {}
 
+    ###     retrieve and check the arguments used in command line
+    print("###  Check the extensions of the files")
     args = retrieveArguments();
-
+    print("###  Valid extension of the files\n")
+    pastecFile=args.classif
     ####    Reading of the classif file ####
     try:
-        pastecFile=args.classif # take the second argument in the terminal command as file name
+        # take the second argument in the terminal command as file name
+        print("###  Read of the classif file")
         pastec=readPastec(pastecFile, nonTE, potentialChimeric, noCat, TE)
+        print("###  End of reading of the classif file\n")
     except IndexError:
-        print("No PASTEC provided")
+        print("###  No PASTEC provided\n### Classification aborted")
         sys.exit(1)
 
     # print("TE : %d, noCat : %d, nonTE : %d" %(len(TE), len(noCat), len(nonTE)))
@@ -318,12 +323,14 @@ if __name__ == "__main__":
     #     pass
     #
     ####    Reading of the fasta file ####
-    # try:
-    #     fastaFile=sys.argv[2] # take the second argument in the terminal command as file name
-    #     fasta=readFasta(fastaFile)
-    # except IndexError:
-    #     print("No Fasta provided")
-    #     sys.exit(1)
+    try:
+        print("###  Read of the FASTA file")
+        fastaFile=sys.argv[2] # take the second argument in the terminal command as file name
+        fasta=readFasta(fastaFile)
+        print("###  End of reading of the FASTA file\n")
+    except IndexError:
+        print("###  No Fasta provided\n###  Classification aborted")
+        sys.exit(1)
     # for key in fasta.keys():
-    #     print(fasta[key]["seq"])
+    #     print(">%s :\n%s\n" %(key, fasta[key]["seq"]))
     # save(fasta, nonTE, potentialChimeric, noCat, TE)
