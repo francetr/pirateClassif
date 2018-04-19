cf5f06f308d87f9f2ee8c6bb9303b1acab53727c
francetr <tristan.frances@laposte.net>
2018-04-19 15:49:43 +0200

    suppression mot cle de l'ancienne version de DB

--- scriptClassif.py
+++ scriptClassif.py
@@ -209,7 +209,7 @@ def categorization(SEQUENCE, NONTE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
 	classDetermination(features, nonTE, potentialChimeric,  noCat, TE, BASELINE)
 
 
-def classDetermination(FEATURES, NONTE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
+def classDetermination(SEQUENCE, NONTE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
 	"""
 	Determine the class of a sequence.
 	For this, complete four dictionnaries, passed onto arguments, that will contain the different catagories that caracterize the sequence.
@@ -229,38 +229,41 @@ def classDetermination(FEATURES, NONTE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
 	@return: None.
 	"""
 	####	Check first if the sequence is chimeric
-	if FEATURES[3] != "PotentialChimeric":
+	if SEQUENCE[3] != "PotentialChimeric":
 		####	Check the class of the sequence if it is not chimeric : class I
-		if FEATURES[4] == "I" :
-			TE[FEATURES[0]]={"class":"I"}
-			# print(FEATURES[0])
-			orderDetermination(FEATURES, POTENTIALCHIMERIC, NOCAT, TE, BASELINE)
+		if SEQUENCE[4] == "I" :
+			TE[SEQUENCE[0]]={"class":"I"}
+			# print(SEQUENCE[0])
+			orderDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE)
+			pass
 		####	classII
-		elif FEATURES[4] == "II":
-			TE[FEATURES[0]]={"class":"II"}
-			orderDetermination(FEATURES, POTENTIALCHIMERIC, NOCAT, TE, BASELINE)
-			# print(FEATURES[0])
+		elif SEQUENCE[4] == "II":
+			TE[SEQUENCE[0]]={"class":"II"}
+			orderDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE)
+			# print(SEQUENCE[0])
+			pass
 		####	NoCat
-		elif FEATURES[4] == "noCat":
-			NOCAT[FEATURES[0]]={"class":"unknown"}
-			# print("NoCat : %s %s" % (FEATURES[4], FEATURES[5]))
+		elif SEQUENCE[4] == "noCat":
+			NOCAT[SEQUENCE[0]]={"class":"unknown"}
+			# print("NoCat : %s %s" % (SEQUENCE[4], SEQUENCE[5]))
 			# pass
 		####	 NonTE
 		else:
-			NONTE[FEATURES[0]]={"class":"nonTE"}
-			# print("NA : %s %s" % (FEATURES[4], FEATURES[5]))
+			NONTE[SEQUENCE[0]]={"class":"nonTE"}
+			# print("NA : %s %s" % (SEQUENCE[4], SEQUENCE[5]))
+			pass
 	else:
-		POTENTIALCHIMERIC[FEATURES[0]]={"Class":"potentialChimeric"}
-		# print("chimere : %s %s %s" % (FEATURES[0], FEATURES[4], FEATURES[5]))
+		POTENTIALCHIMERIC[SEQUENCE[0]]={"Class":"potentialChimeric"}
+		# print("chimere : %s %s %s" % (SEQUENCE[0], SEQUENCE[4], SEQUENCE[5]))
 
-def orderDetermination(FEATURES, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
+def orderDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
 	"""
 	Determine the order of a sequence. Doing so it complete a dictionnary containing the transposable element.
 	If the order wasn't found, the order's sequence is considered as unknown.
 
 	Keyword arguments:
-	@type FEATURES: list
-	@param FEATURES: names of the features (potentialChimeric, class, order, ...) find in the sequence.
+	@type SEQUENCE: list
+	@param SEQUENCE: name of the list of strings, which contain the sequence to categorize, that will be parsed.
 	@type POTENTIALCHIMERIC: dictionnary
 	@param POTENTIALCHIMERIC: dictionnary for potential chimeric element.
 	@type NOCAT: dictionnary
@@ -273,24 +276,24 @@ def orderDetermination(FEATURES, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
 	@return: None.
 	"""
 	####	 The order of the TE is not determined, the superfamily of the TE will not be determined
-	if FEATURES[5] == "noCat" :
-		TE[FEATURES[0]]["order"]="unknown"
+	if SEQUENCE[5] == "noCat" :
+		TE[SEQUENCE[0]]["order"]="unknown"
 	####	 The order of the TE is a MITE, a LARD or a TRIM : the superfamily of the TE will not be determined
-	elif FEATURES[5] == "MITE" or FEATURES[5] == "LARD" or FEATURES[5] == "TRIM" :
-		TE[FEATURES[0]]["order"]=FEATURES[5]
+	elif SEQUENCE[5] == "MITE" or SEQUENCE[5] == "LARD" or SEQUENCE[5] == "TRIM" :
+		TE[SEQUENCE[0]]["order"]=SEQUENCE[5]
 	####	 The order of the TE is determined : the superfamily of the TE will be determined
 	else:
-		TE[FEATURES[0]]["order"]=FEATURES[5]
-		superFamilyDetermination(FEATURES, POTENTIALCHIMERIC, NOCAT, TE, BASELINE)
+		TE[SEQUENCE[0]]["order"]=SEQUENCE[5]
+		superFamilyDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE)
 
-def superFamilyDetermination(FEATURES, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
+def superFamilyDetermination(SEQUENCE, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
 	"""
 	Determine the superfamily of one sequence. Doing so it complete a dictionnary containing the transposable element.
 	If the superFamily can't be defined, it will be unknown.
 
 	Keyword arguments:
-	@type FEATURES: list
-	@param FEATURES: names of the features (potentialChimeric, class, order, ...) find in the sequence.
+	@type SEQUENCE: list
+	@param SEQUENCE: name of the list of strings, which contain the sequence to categorize, that will be parsed.
 	@type POTENTIALCHIMERIC: dictionnary
 	@param POTENTIALCHIMERIC: dictionnary for potential chimeric element.
 	@type NOCAT: dictionnary
@@ -306,30 +309,30 @@ def superFamilyDetermination(FEATURES, POTENTIALCHIMERIC, NOCAT, TE, BASELINE):
 	"""
 	#TODO deal with sequence that don't have DB comparisons
 	try:
-		####	 search if there is a 'coding' part in the 7th value of FEATURES => needed to defined the FEATURES superfamily. Search for coding(<anything that is not ));>); NB : regex (?!) anything that is not in ()
-		codingRecord = re.search(r'coding=\(((?!\)\);).+?)\);', FEATURES[7]).groups()[0]
-		# codingRecord = re.search(r'coding=\(([^\)]+?)\);?', FEATURES[7]).groups()[0] Another possibility of regex
+		####	 search if there is a 'coding' part in the 7th value of SEQUENCE => needed to defined the SEQUENCE superfamily. Search for coding(<anything that is not ));>); NB : regex (?!) anything that is not in ()
+		codingRecord = re.search(r'coding=\(((?!\)\);).+?)\);', SEQUENCE[7]).groups()[0]
+		# codingRecord = re.search(r'coding=\(([^\)]+?)\);?', SEQUENCE[7]).groups()[0] Another possibility of regex
 		####	Split the coding part to obtain the different results according to the comparison with different databases (3 possibilties: TEBLRtx, TEBLRx and profiles)
 		databaseRecords = codingRecord.split(';')
 		####	Search if there are different names contained in the codingRecord
-		searchDifferentName(FEATURES, TE, databaseRecords, BASELINE)
+		searchDifferentName(SEQUENCE, TE, databaseRecords, BASELINE)
 
-		# print(FEATURES[0], dbName, (matches[FEATURES[0]][dbName]))
+		# print(SEQUENCE[0], dbName, (matches[SEQUENCE[0]][dbName]))
 	except AttributeError as e:
 		####	If there is no coding part : declaration of superfamily as unknown
-		TE[FEATURES[0]]["superfamily"] = "unknown"
-		# NOCAT[FEATURES[0]]=TE[FEATURES[0]]
-		# del TE[FEATURES[0]]
+		TE[SEQUENCE[0]]["superfamily"] = "unknown"
+		# NOCAT[SEQUENCE[0]]=TE[SEQUENCE[0]]
+		# del TE[SEQUENCE[0]]
 		return
 
-def searchDifferentName(FEATURES, TE, DATABASERECORDS, BASELINE):
+def searchDifferentName(SEQUENCE, TE, DATABASERECORDS, BASELINE):
 	"""
 	If the superFamily can't be defined, it will be unknown.
 	The name is recovered from two : the hmm profiles (profiles part) and REPET (TE_BLRx and TE_BLRtx part).
 
 	Keyword arguments:
-	@type FEATURES: list
-	@param FEATURES: names of the features (potentialChimeric, class, order, ...) find in the sequence.
+	@type SEQUENCE: list
+	@param SEQUENCE: name of the list of strings, which contain the sequence to categorize, that will be parsed.
 	@type DATABASERECORDS: list
 	@param DATABASERECORDS: list of string in which there are the superFamily name to search.
 	@type TE: dictionnary
@@ -340,25 +343,25 @@ def searchDifferentName(FEATURES, TE, DATABASERECORDS, BASELINE):
 	@return: None.
 	"""
 	####	Dictionnary that will contains (or not) the different superFamilies find for the concerned sequence
-	# matches={FEATURES[0]:{}}
+	# matches={SEQUENCE[0]:{}}
 	####	List to store the superfamily found for a sequence. Usefull to compare if there are differences between them
 	superFamilyFound=[]
 	####	Scan the different results obtain for each comparisons
 	for dr in DATABASERECORDS:
 		####	Split to obtain the name of the database used (TE_BLRx or TE_BLRtx)
 		dbName = dr.split(':')[0].strip()
-		# matches[FEATURES[0]] = {dbName:[]}
+		# matches[SEQUENCE[0]] = {dbName:[]}
 		####	if profiles is found in coding, search for superFamily name using function searchProfilesName
 		if dbName=='profiles':
-			searchProfilesName(FEATURES, dr, superFamilyFound)
+			searchProfilesName(SEQUENCE, dr, superFamilyFound)
 		####	if TE_BLRx or TE_BLRtx is found in coding, search for superFamily name using function searchRepBaseName
 		elif dbName=="TE_BLRx" or "TE_BLRtx":
-			searchRepBaseName(FEATURES, dr, superFamilyFound)
+			searchRepBaseName(SEQUENCE, dr, superFamilyFound)
 	####	String that will contain the final supefamily name of the sequence
 	finalSuperFamily=""
 	####	if multiple names for superfamily are found tor the sequence, proceed to the comparison between all the names found
 	if len(superFamilyFound) > 1:
-		finalSuperFamily=superFamilyComparison(FEATURES, superFamilyFound, BASELINE)
+		finalSuperFamily=superFamilyComparison(SEQUENCE, superFamilyFound, BASELINE)
 	####	One superFamily name have been found
 	elif len(superFamilyFound) == 1:
 		####	Replace the superfamily name by "unknown"
@@ -369,17 +372,17 @@ def searchDifferentName(FEATURES, TE, DATABASERECORDS, BASELINE):
 	####	No superFamily name have been found
 	else:
 		finalSuperFamily="unknown"
-	# matches[FEATURES[0]][dbName].append(searchObj.groups())
-	TE[FEATURES[0]]["superFamily"]=finalSuperFamily
+	# matches[SEQUENCE[0]][dbName].append(searchObj.groups())
+	TE[SEQUENCE[0]]["superFamily"]=finalSuperFamily
 
-def searchProfilesName(FEATURES, DATABASERECORD, SUPERFAMILYFOUND):
+def searchProfilesName(SEQUENCE, DATABASERECORD, SUPERFAMILYFOUND):
 	"""
 
 	Search the keywords in the profiles part of coding.
 
 	Keyword arguments:
-	@type FEATURES: list
-	@param FEATURES: names of the features (potentialChimeric, class, order, ...) find in the sequence.
+	@type SEQUENCE: list
+	@param SEQUENCE: name of the list of strings, which contain the sequence to categorize, that will be parsed.
 	@type DATABASERECORD: string
 	@param DATABASERECORD: profiles that will be parsed to find the keywords.
 	@type SUPERFAMILYFOUND: list
@@ -387,33 +390,25 @@ def searchProfilesName(FEATURES, DATABASERECORD, SUPERFAMILYFOUND):
 
 	@return: TODO
 	"""
-	#TODO try to differenciate different possible syntax according the first regex found. Ex: PiRATEdb_CACTA_Tase_NA or _RT_reina_NA_RT_NA or PF05699.9_Dimer_Tnp_hAT_NA_Tase_21.4
+	#TODO
 	# print(DATABASERECORD)
-	####	first split to get rid of profiles:
-	DATABASERECORD=DATABASERECORD.split("profiles:")
-	####	Parse all the results of profiles and search for regex
-	for substr in DATABASERECORD[1].split(','):
-		# print(substr+"\n")
+	for substr in DATABASERECORD.split(','):
+		# print(substr)
 		try:
-			searchObj = re.search(r'(?:profiles:)? _?([^_]+)_([^_]+)_([^_]+)_([^:]+): ([0-9\.]+)%\(([0-9\.]+)%\)', substr)
-			# print(FEATURES[0], searchObj.groups()[0])
-			####	Proceed to different operations according the first string of the first regex
-			####	If first regex is PiRATEdb
-			if searchObj.groups()[0] == "PiRATEdb":
-				print("Base de Données PiRATE : ", FEATURES[0], searchObj.groups()[1], searchObj.groups()[2])
-			# print(searchObj.groups())
+			searchObj = re.search(r' ?(?:profiles)?:?([^_]+)_([^_]+)_([^_]+)_([^:]+): ([0-9\.])%\([0-9.]%', substr)
+			# print(searchObj.groups()[1])
 		except AttributeError:
-			print('Issue during searching profiles on : '+ substr + "\n")
+			print('Issue during searching profiles on : '+ substr)
 
 
-def searchRepBaseName(FEATURES, DATABASERECORD, SUPERFAMILYFOUND):
+def searchRepBaseName(SEQUENCE, DATABASERECORD, SUPERFAMILYFOUND):
 	"""
 
 	Search the keywords in the TE_BLRx and TE_BLRtx part of coding.
 
 	Keyword arguments:
-	@type FEATURES: list
-	@param FEATURES: names of the features (potentialChimeric, class, order, ...) find in the sequence.
+	@type SEQUENCE: list
+	@param SEQUENCE: name of the list of strings, which contain the sequence to categorize, that will be parsed.
 	@type DATABASERECORD: string
 	@param DATABASERECORD: profiles that will be parsed to find the keywords.
 	@type SUPERFAMILYFOUND: list
@@ -426,22 +421,22 @@ def searchRepBaseName(FEATURES, DATABASERECORD, SUPERFAMILYFOUND):
 			####	regex split into groups. Group's number correspondance : 0 : name of the sequence; 1 : type of class; 2 : type of order; 3 : superFamily name
 			searchObj = re.search(r' ([^:]+):(?:Class)?(I+|\?):([^:]+):([^:]+): ([0-9\.]+)%', substr)
 			SUPERFAMILYFOUND.append(searchObj.groups()[3])
-			# print(FEATURES[0], searchObj.groups()[1], searchObj.groups()[2], searchObj.groups()[3])
+			# print(SEQUENCE[0], searchObj.groups()[1], searchObj.groups()[2], searchObj.groups()[3])
 			####	TODO If a sequence superfamily haven't been determined.
 			if searchObj.groups()[3] == "?":
-				TE[FEATURES[0]]["superFamily"]="unknown"
+				TE[SEQUENCE[0]]["superFamily"]="unknown"
 		except AttributeError:
 			print('Issue during searching RepBase name on : '+ substr)
 
 
-def superFamilyComparison(FEATURES, SUPERFAMILYFOUND, BASELINE):
+def superFamilyComparison(SEQUENCE, SUPERFAMILYFOUND, BASELINE):
 	"""
 
 	Compare the different superFamilies found for one sequence. For this uses the method combinations of the package itertools allowing to combine 2 by 2 the different elements of a list.
 
 	Keyword arguments:
-	@type FEATURES: list
-	@param FEATURES: names of the features (potentialChimeric, class, order, ...) find in the sequence.
+	@type SEQUENCE: list
+	@param SEQUENCE: name of the list of strings, which contain the sequence to categorize, that will be parsed.
 	@type SUPERFAMILYFOUND: list
 	@param SUPERFAMILYFOUND: names of the superfamily found for one sequence during the superFamilyDetermination.
 	@type BASELINE: dictionnary
@@ -466,7 +461,7 @@ def superFamilyComparison(FEATURES, SUPERFAMILYFOUND, BASELINE):
 			####	If the superfamily name is already in the dictionnary and exists in the BASELINE, its counter is incremented by 1
 			elif (superFamily in BASELINE["specific"][possibleName]) and (superFamily in superFamilyCount.keys()):
 				superFamilyCount[superFamily]+=1
-	# print(FEATURES[0], superFamilyCount)
+	# print(SEQUENCE[0], superFamilyCount)
 
 	name=""
 	max = 0
@@ -484,9 +479,9 @@ def superFamilyComparison(FEATURES, SUPERFAMILYFOUND, BASELINE):
 	if name=="?":
 		name="unknown"
 
-	# print(FEATURES[0], name, max)
+	# print(SEQUENCE[0], name, max)
 	return name
-	# print("{}, {}, count: {}, len: {}".format(FEATURES[0], SUPERFAMILYFOUND, superFamilyCount, len(SUPERFAMILYFOUND)))
+	# print("{}, {}, count: {}, len: {}".format(SEQUENCE[0], SUPERFAMILYFOUND, superFamilyCount, len(SUPERFAMILYFOUND)))
 
 def save(FASTA, NONTE, POTENTIALCHIMERIC, NOCAT, TE):
 	"""
